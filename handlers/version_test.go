6e55f3395c34d857b4de9cc22049907c02f651a5
jorik van looy <jorik716@gmail.com>
2024-11-27 23:31:42 +0100

    feat: add health endpoint
    - add health enpoint
    - turn all endpoint data into json

--- handlers/version_test.go
+++ handlers/version_test.go
@@ -1,12 +1,18 @@
 package handlers
 
 import (
+	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 )
+
+type version struct {
+	Version string `json:"version"`
+}
 
 func TestGetVersion(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/version", nil)
@@ -21,7 +27,15 @@ func TestGetVersion(t *testing.T) {
 	if err != nil {
 		t.Errorf("expected error to be nil, got %v", err)
 	}
-	if string(data) != golangVersion {
-		t.Errorf("Expected v0.0.1 got %v", string(data))
+
+	var endpointVersion version
+
+	err = json.Unmarshal(data, &endpointVersion)
+	if err != nil {
+		log.Fatalf("unexpected error during json Unmarshal: %v", err)
+	}
+
+	if endpointVersion.Version != golangVersion {
+		t.Errorf("Expected v0.0.1 got %v", endpointVersion.Version)
 	}
 }
